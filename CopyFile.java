b1e25cbcea4b562e53a7f81e89d5494f540432a7
AhmedSobhy93 <eng_ahmed9393@yahoo.com>
2016-02-29 19:05:51 +0200

    m

--- CopyFile.java
+++ CopyFile.java
@@ -114,7 +114,26 @@ class CopyFile extends JFrame{
 				}
 			});
 			
-			
+		
+		bro1.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){	
+				
+				JFileChooser fc=new JFileChooser();
+				if(fc.showOpenDialog(CopyFile.this)==JFileChooser.APPROVE_OPTION)
+				{
+					String path=fc.getSelectedFile().getPath();
+					File f1=new File(path);
+					if(f1.exists()){
+						System.out.println("not empty");
+						text1.setText(path);
+						
+					}else{
+						label3.setText("Invalid Source path");
+					}
+				}
+						
+				}
+			});  	
 		
 	
 	public void copyFun(File f1,File f2){
